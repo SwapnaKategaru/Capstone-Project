fadbc844720a66fcece54e5e0fe26dc010ae9d88
Swapna K E <45910799+SwapnaKategaru@users.noreply.github.com>
2021-02-12 14:59:17 +0530

    Add files via upload

---
+++ score(f).py
@@ -0,0 +1,21 @@
+
+import json
+import numpy as np
+import pandas as pd
+import os
+from sklearn.externals import joblib
+def init():
+    global model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.joblib')
+    print("Found model:", os.path.isfile(model_path)) #To check whether the model is actually present on the location we are looking at
+    model = joblib.load(model_path)
+def run(data):
+    try:
+        data = json.loads(data)['data']
+        data = pd.DataFrame.from_dict(data)
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
+
